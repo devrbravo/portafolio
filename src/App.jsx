71b91499188c4044596a5b8920182b72a17ff9e4
Roberto Bravo <ing.sistemas.rbravo@gmail.com>
2024-12-08 10:42:23 -0400

    Modificacion pagina

--- src/App.jsx
+++ src/App.jsx
@@ -125,8 +125,11 @@ const initialCart = () => {
       <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
           <p className="text-white text-center fs-4 mt-4 m-md-0">
-            GuitarLA - Todos los derechos Reservados
-          </p>
+           <ul>GuitarLA - Todos los derechos Reservados</ul> 
+           <ul>Curso UDEMY </ul>
+           <ul>Participante: Roberto Bravo</ul>
+         </p>
+         
         </div>
       </footer>
     </>
